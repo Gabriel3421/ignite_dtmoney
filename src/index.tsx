ac1f074e1e34646ddcf74bc104a7a658839a1d3f
gabriel3421 <gabrielsouza.n.s@hotmail.com>
2021-04-27 19:18:45 -0300

    consumindo api

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createServer } from 'miragejs'
+
+createServer({
+  routes(){
+    this.namespace = 'api';
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'teste',
+          amount: 200,
+          type: 'deposit',
+          category: 'food',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 
 ReactDOM.render(
